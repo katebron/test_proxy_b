71c9f1a907f72b39fe53d09f6e40f42089996685
Bronstad <kate.bronstad@tufts.edu>
2025-01-04 18:19:55 -0500

    add full js file for drupal

---
+++ proxy_link_generator.js
@@ -0,0 +1,77 @@
+/**
+ * @file
+ * Code for proxy_link_generator.js
+ * <!--With many thanks to Washington University St. Louis for the sample code to use for this purpose--> 
+ */
+ 
+(function ($, Drupal, drupalSettings, once) {
+    Drupal.behaviors.ProxyLinkGenerator = {
+      attach: function (context, settings) {
+        $(once('make-link-behavior', '#makeLinkButton', context)).each(function () {
+          $(this).on('click', function() {
+              var oin=$('#edit-in');
+              var oout=$('#edit-out');
+              var intxt=oin.val();
+              if(intxt.length==0) {
+                oin.focus();
+                alert("No URL entered!");
+              } 
+            
+              else {
+                var prep="https://resources.libraries.tufts.edu/?Location=";
+                var rc=intxt.indexOf('https://resources.libraries.tufts.edu/?url=');
+                var rd=intxt.indexOf('.libproxy.tufts.edu');
+                var ezp = intxt.indexOf('ezproxy');
+                var wellFormedHttp=intxt.indexOf('http://')
+                var wellFormedHttps=intxt.indexOf('https://')
+               if(wellFormedHttp=='0'||wellFormedHttps=='0'){
+                  
+                    
+                    if(rc==-1) {
+                    if(rd==-1){
+                              oout.val(prep+intxt);
+                              oout.focus();
+                              oout.select();
+                              if((ezp>=0)||(rd>=0)){
+                                let wording = "This looks like an ezproxy URL. " + 
+                                "Please remove the ezproxy prefix and try again. " +
+                                "You may also want to use the form at " +
+                                "https://resources.libraries.tufts.edu/support/transform";
+                                alert(wording);
+                                oout.val("");
+                                oin.focus();
+                                oin.select();
+                              }  
+                        }
+                    else{
+                        alert("That is a translated URL and shouldn't be used. Click OK and I'll fix it for you");
+                        intxt = intxt.replace(/.libproxy.tufts.edu/g,"")
+                              oout.val(prep+intxt);
+                              oin.focus();
+                              oin.select();
+                        }
+                        }
+                    else {
+                          alert("That is already a proxied URL, so no change is needed");
+                          oout.val("");
+                          oin.focus();
+                          oin.select();
+                    }
+                }
+                else{
+                alert("The URL source URL doesn't start with http:// or https:// or contains multiple entries, please enter a valid URL like http://someaddress.com");
+                      oout.val("");
+                      oin.focus();
+                      oin.select();
+                }
+              }
+            
+                })
+            
+            
+     
+    })
+   }
+  };
+  })(jQuery, Drupal, drupalSettings, once);
+  
